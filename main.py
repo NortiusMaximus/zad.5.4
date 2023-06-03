ec60a0e090157ce53fea998f366b8f7b3fd5babe
Jerzy R <jerzy.roslon@pw.edu.pl>
2023-06-03 20:05:41 +0200

    Add zadanie dodatkowe

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import datetime
+from faker import Faker
+fake = Faker()
 
 class Film:
     instances = []
@@ -22,13 +25,16 @@ class Series(Film):
         super().__init__(*args, **kwargs)
         self.episode_no = episode_no
         self.season_no = season_no
+    
     def __repr__(self):
         return f'{self.title} S{"{:02d}".format(self.season_no)}E{"{:02d}".format(self.episode_no)}' 
-
-F1 = Film('title', '1555', 'genre', 1)
-F2 = Film('atitle2', '15552', 'genre2', 1)
-S1 = Series(2, 4, 'titles', '1444', 'genres', 1,)
-S2 = Series(14, 101, 'atitles2', '14442', 'genres2', 1,)
+    
+    def count_episodes(self):
+        '''
+        Counts no of episodes of provided series.
+        '''
+        filter_series = [x for x in Film.instances if x.title == self.title]
+        return len(filter_series)
 
 def get_movies():
     filter_movies = [x for x in Film.instances if not isinstance(x, Series)]
@@ -56,23 +62,51 @@ def generate_views_x10():
     for i in range(10):
         generate_views()
 
-def top_titles():
-    by_views = sorted(Film.instances, key=lambda movie: movie.plays)
-    return by_views
+def top_titles(how_many, type):
+    '''
+    Returns movies with most views.
 
-generate_views_x10()
-#S1.play(10)
-#print(Film.titles)
-print(S1.plays)
-print(S2.plays)
-print(F1.plays)
-print(F2.plays)
+	Parameters:
+		how_many (int): number of the most viewd movies to be presented on the final list
+		type (int): 1 - films only, 2 - series only, 3 - all types of movies
 
-print(top_titles())
+	Returns:
+		by_views (list): movies with most views according to set parameters
+    '''
+    if type == 1:
+        filter_movies = [x for x in Film.instances if not isinstance(x, Series)]
+        by_views = sorted(filter_movies, key=lambda movie: movie.plays, reverse=True)
+        return by_views[0:how_many]
+    
+    if type == 2:
+        filter_series = [x for x in Film.instances if isinstance(x, Series)]
+        by_views = sorted(filter_series, key=lambda movie: movie.plays, reverse=True)
+        return by_views[0:how_many]
 
+    if type ==3:
+        by_views = sorted(Film.instances, key=lambda movie: movie.plays, reverse=True)
+        return by_views[0:how_many]
+    
+    else:
+        return "Wrong type: 1 - films only, 2 - series only, 3 - all types of movies"
 
-"""
-    Napisz funkcję top_titles(), która zwróci wybraną ilość najpopularniejszych tytułów z biblioteki. 
-    Dla chętnych: dodaj do funkcji parametr content_type, którym wybierzesz czy mają zostać pokazane filmy, czy seriale.
+def add_full_season(series_title, year_aired, genre, season_no, how_many_episodes):
+    list = []
+    for i in range(1,how_many_episodes+1):
+        list.append(i)
+        list[i-1] = Series(i, season_no, series_title, year_aired, genre, 0)
 
-"""
+if __name__ == "__main__":
+    print("Biblioteka filmów")
+    list_of_films = []
+    for i in range(1,11):
+        list_of_films.append(i)
+        list_of_films[i-1] = Film(fake.word(), random.randint(1955, 2023), fake.word(), 0)
+    add_full_season(fake.word(), random.randint(1955, 2023), fake.word(), random.randint(1, 10), random.randint(1, 20))
+    add_full_season(fake.word(), random.randint(1955, 2023), fake.word(), random.randint(1, 10), random.randint(1, 20))
+    for i in range(30):
+        generate_views_x10()
+    now = datetime.datetime.now()
+    print("Najpopularniejsze filmy i seriale dnia", now.strftime("%d-%m-%Y"))
+    print(top_titles(3,3))
+
